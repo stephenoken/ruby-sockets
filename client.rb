95a5b96b560c8ad3af47563ace636155403567cb
Stephen O'Kennedy <stephenoken@gmail.com>
2015-10-05 23:47:06 +0100

    "Tidied up the client and server files"
    ""

--- client.rb
+++ client.rb
@@ -1,58 +1,23 @@
-# require "socket"
-#
-# class Client
-#   def initialize(server)
-#     @server = server
-#     @request = nil
-#     @response = nil
-#     listen
-#     send
-#     @request.join
-#     @response.join
-#   end
-#
-#   def send
-#     puts "Enter username:"
-#     @request = Thread.new do
-#       loop {
-#         msg = $stdin.gets.chomp
-#         @server.puts(msg)
-#       }
-#     end
-#   end
-#
-#   def listen
-#     @response = Thread.new do
-#       loop{
-#         msg = @server.gets.chomp
-#         puts "#{msg}"
-#       }
-#     end
-#   end
-# end
-#
-# server = TCPSocket.open("localhost",3000)
-# Client.new(server)
-
 require "socket"
 
-hostname = 'localhost'
-port = 2000
+class Client
+  def initialize(server)
+    @server = server
+    @server.puts "Connected"
+    @response = nil
+    listen
+    @response.join
+  end
 
-def sendMessage (server)
-  loop {
-    puts "Send Message: "
-    server.puts $stdin.gets.chomp
-  }
+  def listen
+    puts "Hello"
+    @response =Thread.new do
+      loop {
+        puts @server.gets.chomp
+      }
+    end
+  end
 end
 
-s = TCPSocket.open(hostname, port)
-
-s.puts "Connected"
-
-# sendMessage(s)
-
-while line = s.gets
-  puts line.chomp
-end
-s.close
+server = TCPSocket.open("localhost",2000)
+Client.new(server)
--- server.rb
+++ server.rb
@@ -1,50 +1,27 @@
-# require "socket"
-#
-# class Server
-#   def initialize(port,ip)
-#     @server = TCPServer.open(ip,port)
-#     @connections = {}
-#     @rooms = {}
-#     @clients = {}
-#   end
-#
-#   def run
-#     loop {
-#       Thread.start(@server.accept) do | client |
-#         nick_name = client.gets.chomp.to_sym
-#         @connections[:clients].each do | other_name, other_client |
-#           if nick_name == other_name || client == other_client
-#             client.puts "This username already exists"
-#             Thread.kill self
-#           end
-#         end
-#         puts "#{nick_name} #{client}"
-#         @connections[:clients][nick_name] = client
-#         client.puts "Connection established, Thank you for joining! Happy chatting"
-#       end
-#     }
-#   end
-# end
-#
-# server = Server.new(3000,'localhost')
-# server.run()
-
 require "socket"
 
-server = TCPServer.open(2000)
-loop {
-  Thread.start(server.accept) do |client|
-    client.puts(Time.now.ctime)
-     nick_name = client.gets.chomp.to_s
-     puts nick_name.is_a?(String)
-     puts nick_name
-     if nick_name == "Connected"
-       puts "Foo"
-     end
+class Server
+  def initialize(ip,port)
+    @server = TCPServer.open(ip,port)
+    @connections = {}
   end
-}
 
-def endSession(client)
-  client.puts "Closing the connection."
-  client.close
+  def run
+    loop{
+      Thread.start(@server.accept) do |client|
+        client.puts(Time.now.ctime)
+         nick_name = client.gets.chomp.to_s
+         puts nick_name.is_a?(String)
+         puts nick_name
+         if nick_name == "Connected"
+           puts "Foo"
+           client.puts "Welcome #{nick_name}"
+         end
+        #  @connections.add(client)
+      end
+    }
+  end
 end
+
+server = Server.new('localhost',2000)
+server.run()
