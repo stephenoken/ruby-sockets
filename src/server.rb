b7adb31e470088b11fb44b028fdc6780ebf044c0
Stephen O'Kennedy <stephenoken@gmail.com>
2015-11-05 10:00:07 +0000

    "Initial Chatroom joining message"
    ""

--- src/server.rb
+++ src/server.rb
@@ -26,6 +26,7 @@ class Server
   end
 
   def client_connection(client)
+    client.puts "JOINED_CHATROOM:room1\nSERVER_IP:#{@ip}\nPORT:#{@port}"
     loop{
       clientInput = client.gets.chomp.to_s
       puts clientInput
