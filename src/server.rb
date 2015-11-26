caf6a127a19a69a2ab4b7c36bec12ac5ac5a0d88
Stephen O'Kennedy <stephenoken@gmail.com>
2015-11-26 15:20:11 +0000

    "Fixed typo"
    ""

--- src/server.rb
+++ src/server.rb
@@ -103,7 +103,8 @@ class Server
     loop {
       join_id_arguments = get_client_arguments(client)
       arguments = get_client_arguments(client)
-      if arguments[0]=="JOIN_ID"
+      if join_id_arguments[0]=="JOIN_ID"
+        puts "Leaving Chatroom..."
         puts @chatrooms[room_ref].clients[join_id_arguments[2]].client_name
         @chatrooms[room_ref].clients.delete([join_id_arguments[2]])
         puts "Deleted Client:#{@chatrooms[room_ref].clients}"
