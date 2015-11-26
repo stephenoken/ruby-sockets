18a3902ea93554719b30635bdb16334e2f0d66ac
Stephen O'Kennedy <stephenoken@gmail.com>
2015-11-26 16:21:29 +0000

    "Added more tracer statements"
    ""

--- src/server.rb
+++ src/server.rb
@@ -105,7 +105,7 @@ class Server
       arguments = get_client_arguments(client)
       if join_id_arguments[0]=="JOIN_ID"
         puts "Leaving Chatroom..."
-        puts @chatrooms[room_ref].clients[join_id_arguments[2]].client_name
+        puts "Clients name:#{@chatrooms[room_ref].clients[join_id_arguments[2]].client_name}"
         @chatrooms[room_ref].clients.delete([join_id_arguments[2]])
         puts "Deleted Client:#{@chatrooms[room_ref].clients}"
         client.puts "LEFT_CHATROOM:#{room_ref}\nJOIN_ID:#{join_id_arguments[2]}"
