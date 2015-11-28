c70981d6f4be892c712a6a732bfff6b742180bd4
Stephen O'Kennedy <stephenoken@gmail.com>
2015-11-28 17:12:15 +0000

    "Removed commented out code blocks"
    ""

--- src/server.rb
+++ src/server.rb
@@ -49,7 +49,7 @@ class Server
   end
 
   def kill_service(client)
-    # @connections.each { |c| c.close}
+    @connections.each { |_key, thread| thread.close}
     client.close
     @server.close
   end
@@ -69,15 +69,6 @@ class Server
 		puts "Chatroom id: #{chatroom.chatroom_id}"
 		client.puts "JOINED_CHATROOM:#{chatroom_name}\nSERVER_IP:#{@ip}\nPORT:#{@port}\nROOM_REF:#{chatroom.chatroom_id}"
     return chatroom.chatroom_id
-    # loop{
-    #   arguments = get_client_arguments(client)
-    #   command = arguments[0]
-    #   case command
-    #   when "CLIENT_NAME"
-    #     register_client(arguments[2], chatroom, client)
-    #     break
-    #   end
-    # }
 	end
 
 	def register_client(client_name, room_ref, client)
@@ -92,24 +83,9 @@ class Server
     end
     @connections[room_ref + client_name] = client
     puts "#{client} #{@connections}"
-    # puts "#{client} => #{@chatrooms[room_ref].client_threads}"
-    #  @chatrooms[room_ref].client_threads.each do |t|
-    #  end
-		# chatroom_session(client,c_client,chatroom)
 	end
 
 	def chatroom_session(client, c_client, chatroom)
-		# client.puts "CHAT:#{chatroom.chatroom_id}\nCLIENT_NAME:#{c_client.client_name}\nMESSAGE:#{c_client.client_name} has joined this chatroom.\n\n"
-		# loop {
-    #  arguments = get_client_arguments(client)
-    #  puts "Chat Session: #{arguments}"
-		#  command = arguments[0]
-		#  case command
-		#  when "LEAVE_CHATROOM"
-		# 	 leave_chatroom(arguments[2], client)
-    #    break
-		#  end
-    # }
 
 	end
 
